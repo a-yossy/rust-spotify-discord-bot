ac40c2ddca2934ea1e4186b3aaa50bdc6cba38a5
a-yossy <atsuya0930@yahoo.co.jp>
2025-04-15 19:21:26 +0900

    ...

---
+++ src/spotify/artist.rs
@@ -0,0 +1,52 @@
+use anyhow::{Context, Result};
+use rand::{rng, seq::IndexedRandom};
+use reqwest::Client;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct FollowingResponse {
+    artists: Artists,
+}
+
+#[derive(Debug, Deserialize)]
+struct Artists {
+    cursors: Cursors,
+    items: Vec<Artist>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Cursors {
+    after: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Artist {
+    id: String,
+}
+
+pub async fn get_following_random_id(access_token: &str) -> Result<String> {
+    let mut artists_ids = Vec::new();
+    let mut after = Some(String::new());
+    let client = Client::new();
+    while let Some(now_after) = after {
+        let response = client
+            .get("https://api.spotify.com/v1/me/following")
+            .query(&[("type", "artist"), ("after", &now_after)])
+            .bearer_auth(access_token)
+            .send()
+            .await?
+            .json::<FollowingResponse>()
+            .await?;
+        after = response.artists.cursors.after;
+        response
+            .artists
+            .items
+            .into_iter()
+            .for_each(|artist| artists_ids.push(artist.id));
+    }
+
+    Ok(artists_ids
+        .choose(&mut rng())
+        .cloned()
+        .context("アーティストが見つかりません")?)
+}
