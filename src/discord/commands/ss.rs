9531c32a7b41ec4c30870541e29ec29bc94622f1
a-yossy <atsuya0930@yahoo.co.jp>
2025-05-02 00:47:59 +0900

    ...

--- src/discord/commands/ss.rs
+++ src/discord/commands/ss.rs
@@ -1,8 +1,8 @@
 use crate::types::discord::framework::Error;
 use once_cell::sync::Lazy;
+use poise::{CreateReply, ReplyHandle};
 use rig::OneOrMany;
 use rig::message::{AssistantContent, Message, Text, UserContent};
-use serenity::all::EditMessage;
 use std::collections::HashMap;
 use tokio::sync::Mutex;
 
@@ -13,6 +13,7 @@ use super::Context;
 
 pub async fn handle(ctx: Context<'_>, prompt: String) -> Result<(), Error> {
     ctx.defer().await?;
+
     static CONVERSATIONS: Lazy<Mutex<HashMap<u64, Vec<Message>>>> =
         Lazy::new(|| Mutex::new(HashMap::new()));
 
@@ -21,10 +22,7 @@ pub async fn handle(ctx: Context<'_>, prompt: String) -> Result<(), Error> {
 
     let llm_agent = &ctx.data().llm_agent;
 
-    let mut response_stream = llm_agent
-        .stream_chat(&prompt, history.to_vec())
-        .await
-        .expect("プロンプトの読み込みに失敗しました");
+    let mut response_stream = llm_agent.stream_chat(&prompt, history.to_vec()).await?;
     let user_contest = UserContent::Text(Text { text: prompt });
     let user_message = Message::User {
         content: OneOrMany::one(user_contest),
@@ -32,62 +30,48 @@ pub async fn handle(ctx: Context<'_>, prompt: String) -> Result<(), Error> {
     history.push(user_message);
 
     let mut assistant_text = String::new();
-    let mut sent_message: Option<serenity::model::channel::Message> = None;
+    let mut sent_message = None;
     while let Some(chunk) = response_stream.next().await {
-        let _ = ctx.channel_id().broadcast_typing(&ctx.http()).await;
-        match chunk {
-            Ok(rig::streaming::StreamingChoice::Message(text)) => {
+        match chunk? {
+            rig::streaming::StreamingChoice::Message(text) => {
                 assistant_text.push_str(&text);
                 if let Some(ref mut msg_obj) = sent_message {
-                    let builder = EditMessage::new().content(&assistant_text);
-                    let _ = msg_obj.edit(&ctx.http(), builder).await;
+                    let builder = CreateReply::default().content(&assistant_text);
+                    let _ = ReplyHandle::edit(msg_obj, ctx, builder).await;
                 } else {
-                    match ctx.channel_id().say(&ctx.http(), &assistant_text).await {
-                        Ok(m) => sent_message = Some(m),
-                        Err(e) => {
-                            println!("Error sending message: {e:?}");
-                            break;
-                        }
-                    }
+                    let reply_handle = ctx.say(&assistant_text).await?;
+                    sent_message = Some(reply_handle);
                 }
             }
-            Ok(rig::streaming::StreamingChoice::ToolCall(..)) => {
+            rig::streaming::StreamingChoice::ToolCall(..) => {
                 todo!()
-            }
-            // Ok(rig::streaming::StreamingChoice::ToolCall(name, _, param)) => {
-            //     let _ = msg
-            //         .channel_id
-            //         .say(
-            //             &ctx.http,
-            //             format!(
-            //                 "🛠️ **ツール呼び出し**: `{}` \n```json\n{}\n```",
-            //                 name, param
-            //             ),
-            //         )
-            //         .await;
+            } // Ok(rig::streaming::StreamingChoice::ToolCall(name, _, param)) => {
+              //     let _ = msg
+              //         .channel_id
+              //         .say(
+              //             &ctx.http,
+              //             format!(
+              //                 "🛠️ **ツール呼び出し**: `{}` \n```json\n{}\n```",
+              //                 name, param
+              //             ),
+              //         )
+              //         .await;
 
-            //     if let Ok(tool_result) = claude.tools.call(&name, param.to_string()).await {
-            //         let _ = msg
-            //             .channel_id
-            //             .say(
-            //                 &ctx.http,
-            //                 format!("🔍 **ツール結果**:\n```json\n{}\n```", tool_result),
-            //             )
-            //             .await;
+              //     if let Ok(tool_result) = claude.tools.call(&name, param.to_string()).await {
+              //         let _ = msg
+              //             .channel_id
+              //             .say(
+              //                 &ctx.http,
+              //                 format!("🔍 **ツール結果**:\n```json\n{}\n```", tool_result),
+              //             )
+              //             .await;
 
-            //         assistant_text.push_str(&format!(
-            //             "\n\n【ツール `{}` の結果】\n{}",
-            //             name, tool_result
-            //         ));
-            //     }
-            // }
-            Err(e) => {
-                let _ = ctx
-                    .channel_id()
-                    .say(&ctx.http(), format!("[エラー: {e}]"))
-                    .await;
-                break;
-            }
+              //         assistant_text.push_str(&format!(
+              //             "\n\n【ツール `{}` の結果】\n{}",
+              //             name, tool_result
+              //         ));
+              //     }
+              // }
         }
     }
     let assistant_content = AssistantContent::Text(Text {
